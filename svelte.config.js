2607c20b8246ca7525ec1005b48a7919fea7aad0
sayfetik <sayfetik2005@gmail.com>
2024-07-06 23:56:34 +0300

    update

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,16 @@
 import adapter from '@sveltejs/adapter-static';
 
 const config = {
-  kit: {
-    adapter: adapter({
-      // включите fallback для SPA
-      fallback: 'app.html'
-    }),
-    paths: {
-      // Укажите ваш базовый путь, например "/FWD_Adilia_Saifetdiarova"
-      base: process.env.NODE_ENV === 'production' ? '/FWD_Adilia_Saifetdiarova' : '',
+    kit: {
+        adapter: adapter({
+            // Включите 'fallback' если используете SPA
+            fallback: 'index.html'
+        }),
+        paths: {
+            // Укажите ваш базовый путь, например "/FWD_Adilia_Saifetdiarova"
+            base: process.env.NODE_ENV === 'production' ? '/FWD_Adilia_Saifetdiarova' : '',
+        }
     }
-  }
 };
 
 export default config;
